29af030b33869a634a9480318847c6a5d6dac36a
TomVasile <43349666+TomVasile@users.noreply.github.com>
2023-12-30 21:38:48 -0600

    first commit: making it my own

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,6 +29,10 @@ return {
       lspconfig.lua_ls.setup({
         capabilites = capabilities,
       })
+      lspconfig.rust_analyzer.setup({
+        capabilites = capabilities,
+      })
+
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
