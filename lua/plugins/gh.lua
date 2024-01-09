cb692bd0a8571220dab9a564f4f080ae58f18cc0
TomVasile <43349666+TomVasile@users.noreply.github.com>
2024-01-09 01:06:11 -0600

    formatting

--- lua/plugins/gh.lua
+++ lua/plugins/gh.lua
@@ -1,16 +1,16 @@
 return {
-  'pwntester/octo.nvim',
-  lazy = false,
-  init = function()
-    local octo = require("octo")
+    'pwntester/octo.nvim',
+    lazy = false,
+    init = function()
+        local octo = require("octo")
 
-    -- REQUIRED
-    octo:setup()
-    -- REQUIRED
-  end,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    'nvim-tree/nvim-web-devicons',
-  },
+        -- REQUIRED
+        octo:setup()
+        -- REQUIRED
+    end,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
 }
