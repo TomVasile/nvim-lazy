ad74f7c3e6578b17dbc7874b85ef710b09a28877
TomVasile <43349666+TomVasile@users.noreply.github.com>
2024-01-15 12:15:04 -0600

    remove prev, next

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -16,10 +16,6 @@ return {
         vim.keymap.set("n", "<C-2", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
     end,
     dependencies = { "nvim-lua/plenary.nvim" },
 }
